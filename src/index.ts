ebfddd6609ef10ba66aa724b28754903d5f84f82
Oleg Semyonov <oleg@prezly.com>
2023-04-24 15:59:29 +0400

    Add named `translations` export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 // eslint-disable-next-line no-restricted-exports, import/no-default-export
 export { default } from './messageDescriptors';
+export { default as translations } from './messageDescriptors';
